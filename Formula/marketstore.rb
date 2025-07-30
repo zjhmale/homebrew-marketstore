b699a0e8017fd0bdd830a1d736a1027b0439cba9
sjnscythe <103406802+sjnscythe@users.noreply.github.com>
2025-07-30 08:36:34 +0530

    Update marketstore.rb

--- Formula/marketstore.rb
+++ Formula/marketstore.rb
@@ -2,8 +2,11 @@ class Marketstore < Formula
   desc "Backdoored Marketstore for testing"
   homepage "https://example.com"
   url "https://example.com/fake.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   version "0.0.1"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"  # empty file hash
+
+  # 👇 Add this line to support --HEAD
+  head "https://github.com/zjhmale/homebrew-marketstore.git", branch: "main"
 
   def install
     cwd = `pwd`.chomp
